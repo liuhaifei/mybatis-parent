59039943dcdbd6b0236ac32c96faca79687f26d2
刘海飞 <15901995342@163.com>
2018-07-27 18:17:09 +0800

    mybatis test

---
+++ src/main/java/com/xinho/mybatis/MExecutor.java
@@ -0,0 +1,12 @@
+package com.xinho.mybatis;
+
+/**
+ * @author lhf
+ * @Title: ${file_name}
+ * @Package ${package_name}
+ * @Description: ${todo}
+ * @date 2018/7/2717:38
+ */
+public interface MExecutor {
+    <E> E query(String statement,String paramter);
+}
---
+++ src/main/java/com/xinho/mybatis/MSimpleExecutor.java
@@ -0,0 +1,15 @@
+package com.xinho.mybatis;
+
+/**
+ * @author lhf
+ * @Title: ${file_name}
+ * @Package ${package_name}
+ * @Description: ${todo}
+ * @date 2018/7/2717:39
+ */
+public class MSimpleExecutor implements MExecutor {
+    @Override
+    public <E> E query(String statement, String paramter) {
+        return null;
+    }
+}
